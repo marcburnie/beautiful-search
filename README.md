8febb8b185f01aa7973c9a0050fb82238a7049d6
Grace Park <31391831+gracepark01@users.noreply.github.com>
2020-07-23 21:59:14 -0500

    Cleaned up files and fixed minor display bugs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# hackaway
+# Search Inspiration Chrome Extension
+
+This Chrome extension prettifies your Google search page with beautiful background photos and inspirational quotes. Download the
--- main.js
+++ main.js
@@ -1,18 +1,16 @@
+// removes div that causes a blank strip across webpage
 const box = $('.sfbg');
 box.remove();
 
-$.get('https://picsum.photos/list', (result) => {
-  // const result = JSON.parse(response);
-  console.log(result, 'result');
+// random number generator
+const randomize = (len) => {
+  return Math.floor(Math.random() * len);
+};
 
-  function getRandomArbitrary() {
-    return Math.floor(Math.random() * result.length);
-  }
-
-  let random = getRandomArbitrary();
-  console.log(random, 'random');
-  let image = 'https://unsplash.it/1700/1000?image=' + result[random].id;
-  console.log(image, 'image');
+// gets random photo from photo API and displays it as background to webpage
+$.get('https://picsum.photos/list', (response) => {
+  let randomImage = randomize(response.length);
+  let image = 'https://unsplash.it/1900/1000?image=' + response[randomImage].id;
 
   $('#viewport').css(
     'background',
@@ -20,21 +18,22 @@ $.get('https://picsum.photos/list', (result) => {
   );
 });
 
-setTimeout(quotation, 1000);
-
-function quotation() {
+// gets random quote from quote API and displays it on webpage
+const quotation = () => {
   $.get('https://type.fit/api/quotes', (response) => {
     const data = JSON.parse(response);
-    function randomize() {
-      return Math.floor(Math.random() * data.length);
-    }
-    let randomQuote = randomize();
+    let randomQuote = randomize(data.length);
 
     if (data[randomQuote].text.length > 80) return quotation();
 
-    $('#searchform').prepend(
-      `<center><blockquote id="quote"> <span class="color">${data[randomQuote].text}</span></blockquote></center> <center><p><span class="color"> ${data[randomQuote].author} </span></p></center> `
+    let author = data[randomQuote].author;
+    if (author === null) author = 'Unknown';
+
+    $('div#searchform.jhp').prepend(
+      `<center><blockquote id="quote"> <span class="color">${data[randomQuote].text}</span></blockquote></center> <center><p><span class="color"> ~ ${author} ~</span></p></center> `
     );
-    // console.log(data[randomQuote], 'dataRandom');
   });
-}
+};
+
+// sends quote function to web API to give photo time to load
+setTimeout(quotation, 1000);
